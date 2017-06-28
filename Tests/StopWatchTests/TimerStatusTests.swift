b654e6dbd44292135713597eeb133768ef47a3e1
Matej Košiarčik <matej@binarytrex.com>
2017-06-28 10:53:25 +0200

    [ADD][REF] Completely rework time keeping mechanism

---
+++ Tests/StopWatchTests/TimerStatusTests.swift
@@ -0,0 +1,26 @@
+//
+// TimerTests.swift
+// Copyright © 2017 Matej Kosiarcik. All rights reserved.
+//
+
+import Nimble
+import XCTest
+@testable import StopWatch
+
+final class TimerStatusTest: XCTestCase {}
+
+extension TimerStatusTest {
+    func testEquatability() {
+        // given
+        let date = Date()
+        let reference: [StopWatch.Timer.Status] = [.stopped, .running(date)]
+        let testers: [StopWatch.Timer.Status] = [.stopped, .running(date), .running(Date())]
+        let expected = [[true, false, false], [false, true, false]].flatMap { $0 }
+
+        // when
+        let tested = reference.flatMap { ref in testers.flatMap { ref == $0 } }
+
+        // then
+        expect(tested) == expected
+    }
+}
---
+++ Tests/StopWatchTests/TimerTests.swift
@@ -0,0 +1,40 @@
+//
+// TimerTests.swift
+// Copyright © 2017 Matej Kosiarcik. All rights reserved.
+//
+
+import Nimble
+import XCTest
+@testable import StopWatch
+
+final class TimerTest: XCTestCase {}
+
+extension TimerTest {
+    func testInitialization() {
+        // when
+        let timer = StopWatch.Timer()
+
+        // then
+        expect(timer.current) == 0
+        expect(timer.laps) == []
+        XCTAssertEqual(timer.status, .stopped)
+    }
+
+    func testStarting() {
+        // given
+        var timer = StopWatch.Timer()
+
+        // when
+        timer.start()
+
+        // then
+        let exp = expectation(description: "")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { exp.fulfill() }
+        wait(for: [exp], timeout: 1)
+        expect(timer.current).to(beCloseTo(0.3, within: 0.01))
+        switch timer.status {
+        case .running(_): break
+        default: XCTFail()
+        }
+    }
+}
