74d32695a537a5dc48f24450830797fbd8c95d37
Matej Košiarčik <matej@binarytrex.com>
2017-07-17 18:13:16 +0200

    [REF] Move command checking to separate function to simplify code

--- Tools/helpers.sh
+++ Tools/helpers.sh
@@ -15,3 +15,22 @@ strip() {
     printf "%s\n" "$(cat -s "${1}")" >"${1}"                   # strip multiple empty lines and trailing newlines
     printf "%s\n" "$(sed 's~[[:space:]]*$~~' <"${1}")" >"${1}" # remove trailing whitespace
 }
+
+# searches for passed command
+# returns 0 if command was found, non-0 otherwise
+# accepts single argument of command to look up
+exists() {
+    command -v "${1}" >/dev/null 2>&1
+}
+
+# checks if given tool exists
+# print message if not
+# accepts single argument of command to look up
+check() (
+    exists "${1}"
+    doesExists="${?}"
+    if [ ! "${doesExists}" -eq "0" ]; then
+        printf "%s\n" "warning: ${1} not found. (tool_not_found)"
+    fi
+    return "${doesExists}"
+)
