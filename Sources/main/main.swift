eb234577738a802a450a1327a17827608f156b2d
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-07-31 15:41:31 +0200

    [REF] Remove newlines at end of output

--- Sources/main/main.swift
+++ Sources/main/main.swift
@@ -8,7 +8,12 @@ import Foundation
 
 var helperOutput = ""
 let result = Program.new(for: CommandLine.arguments).map { $0.main(output: &helperOutput) }
+helperOutput = helperOutput.trimmingCharacters(in: .whitespacesAndNewlines)
 switch result {
-case .success(let exitCode): print(helperOutput); exit(exitCode)
-case .failure(let error): print(error.description); exit(1)
+case .success(let exitCode):
+    if helperOutput != "" { print(helperOutput) }
+    exit(exitCode)
+case .failure(let error):
+    print(error.description)
+    exit(1)
 }
