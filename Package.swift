14a6c2edbb59a2f4ca8e31b5166a2f03156d022c
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-12-14 12:55:10 +0100

    [CONF] Remove package header notice

--- Package.swift
+++ Package.swift
@@ -1,9 +1,4 @@
 // swift-tools-version:4.0
-//
-// This file is part of Stopwatch which is released under MIT license.
-// See file LICENSE.txt or go to https://github.com/matejkosiarcik/Stopwatch for full license details.
-//
-
 import PackageDescription
 
 let package = Package(name: "Stopwatch")
