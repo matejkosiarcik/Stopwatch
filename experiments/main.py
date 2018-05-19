8e78e0bdfa9c3bc7b1298fb4d131c298f516afbc
Matej Košiarčik <matejkosiarcik@gmail.com>
2018-05-19 20:09:16 +0200

    [ADD] python basic reimplementation

---
+++ experiments/main.py
@@ -0,0 +1,129 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import argparse
+import datetime
+import sys
+import termios
+import threading
+import time
+
+
+class Timer:
+    @property
+    def is_running(self):
+        return self._is_running
+
+    def __init__(self, callback):
+        self._accumulated_absolute = datetime.timedelta()
+        self._accumulated_relative = datetime.timedelta()
+        self._callback = callback
+        self._is_running = False
+        self._lap = 1
+        self._last_now = datetime.datetime.utcfromtimestamp(0)
+        self._last_start = datetime.datetime.utcfromtimestamp(0)
+        self._thread = threading.Thread()
+
+    def start(self):
+        assert not self._is_running
+        self._is_running = True
+        self._last_start = datetime.datetime.utcnow()
+        self._thread = threading.Thread(target=self._loop)
+        self._thread.start()
+
+    def stop(self):
+        assert self._is_running
+        self._is_running = False
+        self._thread.join()
+        self._accumulated_relative += (self._last_now - self._last_start)
+        self._accumulated_absolute += (self._last_now - self._last_start)
+
+    def toggle(self):
+        if self._is_running:
+            self.stop()
+        else:
+            self.start()
+
+    def lap(self):
+        # technically we could update even when running
+        # but it is safer/easier not to
+        # mainly regarding printing (newlines would mangle)
+        assert not self._is_running
+        self._lap += 1
+        self._accumulated_relative = datetime.timedelta()
+        self._callback(self._lap, self._accumulated_relative,
+                       self._accumulated_absolute)
+
+    def _loop(self):
+        while self.is_running:
+            self._last_now = datetime.datetime.utcnow()
+            self._callback(self._lap, self._accumulated_relative +
+                           (self._last_now - self._last_start),
+                           self._accumulated_absolute +
+                           (self._last_now - self._last_start))
+            time.sleep(0.001)
+
+
+def main(arguments):
+    """Main script function"""
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    # TODO: add quiet, usage
+    arguments = parser.parse_args(arguments[1:])
+
+    # print usage
+    print("Controls:\n"
+          "  <Enter>      - new lap\n"
+          "  <Space>      - pause/continue\n"
+          "  <ESC> or <Q> - quit\n")
+    print("        split-time     total-time")
+    print("       ------------   ------------")
+
+    # well, get to work
+    program()
+
+
+def program():
+    # setup
+    attributes = termios.tcgetattr(sys.stdin.fileno())
+    attributes[3] = attributes[3] & ~termios.ECHO
+    attributes[3] = attributes[3] & ~termios.ICANON
+    termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, attributes)
+
+    # function to print values for user
+    def print_update(lap, relative_delta, absolute_delta):
+        sys.stdout.write("\rlap " + str(lap) + ": " +
+                         delta_formatted(relative_delta) + " - " +
+                         delta_formatted(absolute_delta))
+        sys.stdout.flush()
+
+    timer = Timer(print_update)
+    timer.start()
+
+    while True:
+        command = sys.stdin.read(1).lower()
+        if command == "q" or command == chr(27):
+            if timer.is_running:
+                timer.stop()
+            break
+        elif command == " ":
+            timer.toggle()
+        elif command == "\r" or command == "\n":
+            should_restart = timer.is_running
+            if should_restart:
+                timer.stop()
+            sys.stdout.write("\n")
+            timer.lap()
+            if should_restart:
+                timer.start()
+    sys.stdout.write("\n")
+
+
+def delta_formatted(delta):
+    milliseconds = str(delta.microseconds // 1000).rjust(3, str("0"))
+    seconds = str(delta.seconds % 60).rjust(2, str("0"))
+    minutes = str((delta.seconds // 60) % 60).rjust(2, str("0"))
+    hours = str(delta.seconds // 3600 + delta.days * 24).rjust(2, str("0"))
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ experiments/tests.py
@@ -0,0 +1,100 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import datetime
+import time
+import unittest
+import main
+
+
+class MainTests(unittest.TestCase):
+
+    def test_delta_description(self):
+        data = [
+            (datetime.timedelta(), "00:00:00.000"),
+            (datetime.timedelta(0, 1, 123456), "00:00:01.123"),
+            (datetime.timedelta(0, 5000, 123456), "01:23:20.123"),
+            (datetime.timedelta(1, 3700, 123456), "25:01:40.123"),
+        ]
+
+        for entry in data:
+            self.assertEqual(main.delta_formatted(entry[0]), entry[1])
+
+
+class TimerTests(unittest.TestCase):
+
+    def _help_test_timer(self, operations):
+        data = {
+            'called': False,
+            'lap': 0,
+            'time_relative': 0.0,
+            'time_absolute': 0.0,
+        }
+
+        def update(lap, delta_relative, delta_absolute):
+            data['called'] = True
+            data['lap'] = lap
+            data['time_relative'] = delta_relative.total_seconds()
+            data['time_absolute'] = delta_absolute.total_seconds()
+
+        timer = main.Timer(update)
+        operations(timer)
+        return data
+
+    def test_timer_basic(self):
+        def operations(timer):
+            timer.start()
+            time.sleep(0.02)
+            timer.stop()
+
+        data = self._help_test_timer(operations)
+
+        self.assertEqual(data['lap'], 1)
+        self.assertAlmostEqual(data['time_relative'], 0.02, delta=0.01)
+        self.assertAlmostEqual(data['time_absolute'], 0.02, delta=0.01)
+
+    def test_timer_pausing(self):
+        def operations(timer):
+            timer.start()
+            time.sleep(0.01)
+            timer.stop()
+            time.sleep(0.01)
+            timer.start()
+            time.sleep(0.01)
+            timer.stop()
+
+        data = self._help_test_timer(operations)
+
+        self.assertEqual(data['lap'], 1)
+        self.assertAlmostEqual(data['time_relative'], 0.02, delta=0.01)
+        self.assertAlmostEqual(data['time_absolute'], 0.02, delta=0.01)
+
+    def test_timer_lapping_simple(self):
+        def operations(timer):
+            timer.start()
+            timer.stop()
+            timer.lap()
+
+        data = self._help_test_timer(operations)
+
+        self.assertEqual(data['lap'], 2)
+        self.assertAlmostEqual(data['time_relative'], 0, delta=0.002)
+        self.assertAlmostEqual(data['time_absolute'], 0, delta=0.002)
+
+    def test_timer_lapping_complex(self):
+        def operations(timer):
+            timer.start()
+            time.sleep(0.01)
+            timer.stop()
+            timer.lap()
+            timer.start()
+            time.sleep(0.01)
+            timer.stop()
+
+        data = self._help_test_timer(operations)
+
+        self.assertEqual(data['lap'], 2)
+        self.assertAlmostEqual(data['time_relative'], 0.01, delta=0.01)
+        self.assertAlmostEqual(data['time_absolute'], 0.02, delta=0.01)
+
+
+if __name__ == '__main__':
+    unittest.main()
